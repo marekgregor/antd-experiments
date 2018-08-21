492bcedfc48cfa757f91fd2ce8137acefd0f9af0
Marek Gregor <marek.gregor@gmail.com>
2018-08-21 08:43:33 +0200

    Test with autosizing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import moment from "moment";
 import { Modal, Button, Table } from "antd";
+import { AutoSizer } from "react-virtualized";
+import Measure from "react-measure";
+
 import "antd/dist/antd.css";
 import "./style.css";
 
@@ -16,6 +19,9 @@ const columns = [{
 }, {
   title: 'Address',
   dataIndex: 'address',
+}, {
+  title: 'Address 2',
+  dataIndex: 'address2',
 }];
 
 const data = [];
@@ -25,19 +31,57 @@ for (let i = 0; i < 100; i++) {
     name: `Edward King ${i}`,
     age: 32,
     address: `London, Park Lane no. ${i}`,
+    address2: `A little bit longer and longer and longer and longer and longer adress for table - London, Park Lane no. ${i}`,
   });
 }
 
 class App extends React.Component {
-  state = { visible: false };
-  
+  state = {
+    visible: false, columns, dimensions: {
+      width: -1,
+      height: -1
+    }
+  };
+
   render() {
+    const { width, height } = this.state.dimensions;
     return (
       <div>
-        <Table columns={columns} dataSource={data} pagination={{ pageSize: 20 }} 
-        scroll={{x: true}} />, 
+        <AutoSizer disableHeight>
+          {
+            ({ width, height }) => {
+              console.log(`Available width: ${width}`);
+              let columns = this.recalculateColumns(this.state.columns);
+              return <Measure
+                bounds scroll
+                onResize={(contentRect) => {
+                  this.setState({ dimensions: contentRect.bounds })
+                  console.log(JSON.stringify(contentRect));
+                }}>
+                {({ measureRef }) =>
+                  <div ref={measureRef}>
+                    <Table columns={columns} dataSource={data}
+                      pagination={{ pageSize: 10 }} />
+                     {/*console.log(`Width: ${this.state.dimensions}`)*/}
+                  </div>
+                }
+              </Measure>
+            }
+          }
+        </AutoSizer>
       </div>
     );
+  }
+
+  recalculateColumns(columns) {
+    return columns.map((c, columnIndex) => {
+      return Object.assign({}, c,
+        {
+          render: (text, record, rowIndex) => {
+            return `${text}`;
+          }
+        });
+    });
   }
 }
 
